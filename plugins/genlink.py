27a081d0abd6a8d11703aee3bdf27b4ee272253b
Vikkukk <121386987+Vikkukk@users.noreply.github.com>
2022-12-29 19:06:22 +0545

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -40,7 +40,7 @@ async def gen_link_s(bot, message):
 @Client.on_message(filters.command(['batch', 'pbatch']) & filters.create(allowed))
 async def gen_link_batch(bot, message):
     if " " not in message.text:
-        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/GreyMatter_Bots/10 https://t.me/GreyMatter_Bots/20</code>.")
+        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/movierequestbackup1/10 https://t.me/movierequestbackup1/20</code>.")
     links = message.text.strip().split(" ")
     if len(links) != 3:
         return await message.reply("Use correct format.\nExample <code>/batch https://t.me/GreyMatter_Bots/10 https://t.me/GreyMatter_Bots/20</code>.")
